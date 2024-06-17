456bf88856915dd73e39f3da1eb21f1f7b4dba36
Aryan <aryanmusicbht@gmail.com>
2024-06-17 17:33:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("red").addEventListener("click",()=>{
+    document.body.style.backgroundColor = "red"
+})
+document.getElementById("blue").addEventListener("click",()=>{
+    document.body.style.backgroundColor = "blue"
+})
+document.getElementById("purple").addEventListener("click",()=>{
+    document.body.style.backgroundColor = "purple"
+})
+document.getElementById("green").addEventListener("click",()=>{
+    document.body.style.backgroundColor = "green"
+})
+document.getElementById("yellow").addEventListener("click",()=>{
+    document.body.style.backgroundColor = "yellow"
+})
+document.getElementById("greenyellow").addEventListener("click",()=>{
+    document.body.style.backgroundColor = "greenyellow"
+})
+
+document.getElementsByClassName("btn")[0].addEventListener("click",()=>{
+     document.body.style.backgroundColor = "white"
+})
